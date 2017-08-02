e5244dabca4033330df491b07fbfb443cfc55987
augusto.zanetti <augusto.zanetti@BAHAMAS>
2017-08-02 09:46:38 -0300

    Add project files.

---
+++ ProjetoDDD.Infra.Data/Contexto/ProjetoContexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDDD.Infra.Data.Contexto
+{
+    public class ProjetoContexto : DbContext
+    {
+        public ProjetoContexto() 
+            : base("ProjetoDDD") // Nome da string de conexão ou String de conexão
+        {
+
+        }
+
+        //public DbSet<Entidade> Entidades { get; set; }
+    }
+}
---
+++ ProjetoDDD.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoDDD.MVC.Startup))]
+namespace ProjetoDDD.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
